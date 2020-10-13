5697d2bda8b5fbaf0c58996bebb2b121b3ab1477
Victor Trajano <trajanodev@hotmail.com>
2020-10-13 17:46:20 -0400

    Atualização para deploy

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const requireDir = require('require-dir')
+const cors = require('cors')
 
 
 app.use(express.json())
+app.use(cors());
+
 
 //Database connection
 mongoose.connect
